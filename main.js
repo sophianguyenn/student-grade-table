7b4b3fafe097628b70604e893143772a15729287
sophianguyenn <sophiaunguyen@gmail.com>
2020-08-26 03:36:58 +0000

    instantiated PageHeader

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var tableElement = document.querySelector('table');
 var gradeTable = new GradeTable(tableElement);
 
-var studentGrades = new App(gradeTable);
+var headerElement = document.querySelector('header');
+var pageHeader = new PageHeader(headerElement);
+
+var studentGrades = new App(gradeTable, pageHeader);
 studentGrades.start();
